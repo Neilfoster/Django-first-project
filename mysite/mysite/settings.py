2e633b3f34d3009a57e584646bfba79e640a216d
Neilfoster <Neilfoster81@gmail.com>
2020-01-06 20:58:37 +0000

    Cleaned up indenting errors in settings.py and urls.py and added views in views.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -108,7 +108,40 @@ LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'GMT'
 
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
 USE_I18N = True
+
+
+
+
+
+
 
 USE_L10N = True
 
